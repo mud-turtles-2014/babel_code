ef513dff98ec854dd38500b661138d6958d754ac
Katherine <kdmcclin@gmail.com>
2014-10-10 15:14:50 -0400

    remove unnecessary line

--- db/migrate/20141010162604_create_reply_snippets.rb
+++ db/migrate/20141010162604_create_reply_snippets.rb
@@ -3,7 +3,6 @@ class CreateReplySnippets < ActiveRecord::Migration
     create_table :reply_snippets do |t|
       t.text :snippet, null: false
       t.text :description
-      t.integer :vote_count ||= 0
       t.belongs_to :language, null: false
       t.belongs_to :user, null: false
       t.belongs_to :original_snippet, null: false
