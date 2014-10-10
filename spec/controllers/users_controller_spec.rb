4fe7ed0d51f9b22a3f6053d2791c09bab1938dda
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-10 17:14:12 -0400

    Add test for user#new

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,1 +1,25 @@
+require 'spec_helper'
+require 'rails_helper'
 
+describe UsersController do
+  describe "#new" do
+    it "assigns a new user to @user" do
+      get :new
+      expect(assigns(:user)).to be_an_instance_of User
+    end
+    it "renders the #new template"
+  end
+
+  describe "#create" do
+    context "with valid attributes" do
+      it "saves the new user to the database"
+      it "redirects to the login page"
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new user to the database"
+      it "redirects to the #new page"
+      it "assigns a flash to notify user of error"
+    end
+  end
+end
