f9f81438181dbc8a9c2fd626a3733ca92dd44f5e
Sarah Taurchini <sarah.taurchini@gmail.com>
2014-10-10 17:08:32 -0400

    initialize snippet controller/spec

---
+++ spec/original_snippets_controller.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe OriginalSnippetsController do
+
+  describe 'GET #index' do
+  end
+
+  describe 'GET #show' do
+  end
+
+  describe 'GET #new' do
+
+  end
+
+  describe 'GET #edit' do
+  end
+
+  describe "POST #create" do
+  end
+
+  describe 'PUT #update' do
+  end
+
+  describe 'DELETE #destroy' do
+  end
+
+end
