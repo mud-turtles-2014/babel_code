41b2c795ca501bc9e92d0e972c18229a0ecd36ec
Nicolle Quintero <nicolle.quintero@gmail.com>
2014-10-10 17:46:37 -0400

    Add create action to user_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,16 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      redirect_to '/' #change this to login page
+    else
+      redirect_to new_user_path
+    end
+  end
+
   private
 
   def user_params
