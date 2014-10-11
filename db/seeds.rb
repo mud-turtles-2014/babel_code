9d40ae2a3380cf11065b8441834be294878269f3
steph t. zhang <stephtzhang@gmail.com>
2014-10-11 12:39:41 -0400

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create(username: "aferragano", email: "aferragano@gmail.com", password:"password", password_confirmation:"password")
-User.create(username: "katherine", email: "katherine@gmail.com", password:"password", password_confirmation:"password")
+ant = User.create(username: "aferragano", email: "aferragano@gmail.com", password:"password", password_confirmation:"password")
+kat = User.create(username: "katherine", email: "katherine@gmail.com", password:"password", password_confirmation:"password")
 User.create(username: "steph", email: "steph@gmail.com", password:"password", password_confirmation:"password")
+Language.create(language: "Ruby")
+Language.create(language: "JavaScript")
+ant.original_snippets.create(title: "boop1", snippet: "boo boop booopity", description: "lalalallalal", language_id: 1)
+kat.reply_snippets.create(snippet: "katkat", language_id: 2, description: "merpmerpppppp", original_snippet_id: 1)
