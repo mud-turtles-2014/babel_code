0270289f42d1e36ca331392670d788c148e29d10
steph t. zhang <stephtzhang@gmail.com>
2014-10-11 13:52:05 -0400

    Add original_snippet#new with form partial

---
+++ app/controllers/original_snippets_controller.rb
@@ -0,0 +1,36 @@
+class OriginalSnippetsController < ApplicationController
+  # before_filter :current_original_snippet, only: [:show, :update, :edit]
+
+  def new
+    @original_snippet = OriginalSnippet.new
+  end
+
+  def create
+    @original_snippet = current_user.original_snippets.new(original_snippet_params)
+
+    if @original_snippet.save
+      redirect_to original_snippet_path(@original_snippet)
+    else
+      redirect_to new_original_snippet_path
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
+  private
+  def original_snippet_params
+    params.require(:original_snippet).permit( [:title, :description, :snippet, :lanuguage_id])
+  end
+
+end
