89c0e1e5c98a47c5be2e255a77523e6f5bda6d64
Anthony Ferragano <aferragano@gmail.com>
2014-10-12 16:05:41 -0400

    hide show in JS for login and signup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,4 +39,20 @@ $(function() {
     $('.javascript-posts').hide();
     $('.ruby-posts').hide();
   });
+
+
+  function hideShow(buttonID, showItem){
+  $(showItem).hide();
+  $(buttonID).on('click', function(){
+      $(showItem).slideToggle("slow", function(){         
+      });
+    })
+  }
+  hideShow($('#login-btn'),$('#login-div'));
+  hideShow($('#signup-btn'),$('#signup-div'));
+
 })
+
+
+
+
