5149f7550c4869f6c71bb28cd472f78ed7d6362d
steph t. zhang <stephtzhang@gmail.com>
2014-10-12 12:53:20 -0400

    Remove extra comments

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,9 +2,6 @@ class VotesController < ApplicationController
   def new
   end
 
-  # need to change to account for
-  # votable_type and votable_id
-  # instead of reply_snippet
   def create
     current_user.votes.create(votable_id: params[:votable_id], votable_type: params[:votable_type], vote: params[:vote])
     redirect_to :back
