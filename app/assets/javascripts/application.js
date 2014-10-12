6aaea03e32dbfc33b56bc7dbebff2d6d562cd4af
Anthony Ferragano <aferragano@gmail.com>
2014-10-12 16:25:54 -0400

    remove reply view option

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,7 +51,7 @@ $(function() {
   hideShow($('#login-btn'),$('#login-div'));
   hideShow($('#signup-btn'),$('#signup-div'));
   hideShow($('#create-reply-btn'),$('.create-reply'));
-  hideShow($('#all-reply-btn'),$('#all-replies-div'));
+  // hideShow($('#all-reply-btn'),$('#all-replies-div'));
 
 })
 
