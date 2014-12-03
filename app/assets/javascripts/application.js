2ea535ad7ce6024d6672ab737235432a084fd3db
stephtzhang <stephtzhang@gmail.com>
2014-12-03 14:58:16 -0500

    Add ajax for deleting replies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,6 +41,21 @@ $(function() {
     });
   });
 
+  $('.delete-reply').click(function() {
+    var $form = $(this).parent().parent();
+    $form.submit(function(event) {
+      event.preventDefault();
+      var url = $(event.target).attr('action');
+      $.ajax({
+      type: "DELETE",
+      url: url
+      }).done(function(response) {
+        var selector = '.reply-' + response.reply_id;
+        $(selector).remove();
+      });
+    });
+  });
+
   $('#new_reply_snippet').submit(function(event) {
     event.preventDefault();
     var data = $(event.target).serialize();
@@ -51,7 +66,8 @@ $(function() {
     }).done(function(response) {
       $("#all-replies-div").append(response);
     }).success(function() { 
-      console.log("sucesss");
+      $('#reply_snippet_description').val('')
+      $('#reply_snippet_snippet').val('')
       $('.create-reply').hide();
       $('#reply-msg').html('<p>Your reply has been posted</p>');
     });
