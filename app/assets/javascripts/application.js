e440cfd6596c95d5ebd8083fd1963ee04487c920
stephtzhang <stephtzhang@gmail.com>
2015-01-19 13:21:47 -0500

    Replace anonymous functions with named functions in helper scripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,64 +12,22 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require show_posts
+//= require reply_helper
+//= require vote_helper
 
 $(function() {
-  $(".arrow-up").click(function() {
-    $(this).children('form').submit();
-  });
+  $('#signup-div').hide();
 
-  $(".arrow-down").click(function() {
-    $(this).children('form').submit();
-  });
+  $('#login-btn').click(function() {
+    $('#login-div').show();
+    $('#signup-div').hide();
+  })
 
-  $(".vote").submit(function(event) {
-    event.preventDefault();
-    var data = $(event.target).serialize();
-    $.ajax({
-      type: "POST",
-      url: "/votes",
-      data: data,
-      dataType: "json"
-    }).done(function(response) {
-      var div = response.snippet_class + "-" +response.snippet_id + "-" + "votes";
-      $('#' + div).html('<p>' + response.vote_count + '</p>');
-    });
-  });
-
-  $('.delete-reply').click(function() {
-    var $form = $(this).parent().parent();
-    $form.submit(function(event) {
-      event.preventDefault();
-      var url = $(event.target).attr('action');
-      $.ajax({
-      type: "DELETE",
-      url: url
-      }).done(function(response) {
-        var selector = '.reply-' + response.reply_id;
-        $(selector).remove();
-      });
-    });
-  });
-
-  $('#new_reply_snippet').submit(function(event) {
-    event.preventDefault();
-    event.stopPropagation();
-    var data = $(event.target).serialize();
-    $.ajax({
-      type: "POST",
-      url: "/reply_snippets",
-      data: data
-    }).done(function(response) {
-      $("#all-replies-div").append(response);
-    }).success(function() {
-      $('#reply_snippet_description').val('')
-      $('#reply_snippet_snippet').val('')
-      $('.create-reply').hide();
-      $('#reply-msg').html('<p>Your reply has been posted</p>');
-    });
-  });
-
-  var languages = ["recent", "ruby", "javascript", "java", "python"];
+  $('#signup-btn').click(function() {
+    $('#signup-div').show();
+    $('#login-div').hide();
+  })
 
   showPosts("recent");
 
@@ -78,35 +36,18 @@ $(function() {
     showPosts(language);
   });
 
-  function showPosts(language) {
-    for(var i = 0; i < languages.length; ++i) {
-      var divClass = '.' + languages[i] + '-posts';
-      $(divClass).hide();
-    }
-    var divClass = '.' + language + '-posts';
-    $(divClass).show();
-  }
+  $(".arrow-up").click(function() {
+    $(this).children('form').submit();
+  });
 
-  function hideShow(buttonID, showItem){
-  $(showItem).hide();
-  $(buttonID).on('click', function(){
-      $(showItem).slideToggle("slow", function(){
-      });
-    })
-  }
+  $(".arrow-down").click(function() {
+    $(this).children('form').submit();
+  });
 
-  hideShow($('#create-reply-btn'),$('.create-reply'));
-  $('#signup-div').hide();
-  $('#signup-btn').click(function() {
-    $('#signup-div').show();
-    $('#login-div').hide();
-  })
-    $('#login-btn').click(function() {
-    $('#login-div').show();
-    $('#signup-div').hide();
-  })
+  $(".vote").submit(createVote);
+
+  $('.delete-reply').click(deleteReply);
+
+  $('#new_reply_snippet').submit(createReply);
+
 })
-
-
-
-
